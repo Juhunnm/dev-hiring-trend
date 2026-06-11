4925be7ee5a7216147b5bf7b62241bc11e0b2bb9
juhun <minjuhun1@gmail.com>
2026-06-11 19:23:32 +0900

    feat: 상세페이지 크롤링 및 기술스택 추출 구현 - iframe URL 추출 후 직접 접속하여 본문 수집 - TECH_KEYWORDS 사전 기반 기술스택 키워드 추출 - try/except로 오류 발생 시 rollback 처리

---
+++ crawler/saramin_detail.py
@@ -0,0 +1,86 @@
+import csv
+from datetime import datetime 
+from playwright.sync_api import sync_playwright
+
+
+from server.database import SessionLocal
+from server.models import Job
+
+# 본문이랑, 키워드 목록 각각 저장
+BASE_URL = 'https://www.saramin.co.kr'
+
+TECH_KEYWORDS = [
+    "Python", "Java", "C#", "C++", "JavaScript", "TypeScript",
+    "Spring", "React", "Vue", "Next.js", "Django", "FastAPI",
+    "MySQL", "PostgreSQL", "MongoDB", "Redis",
+    "AWS", "Docker", "Kubernetes", "Git"
+]
+
+def extract_keywords(text) :
+    found =[]
+    for keyword in TECH_KEYWORDS:
+        if keyword.lower() in text.lower():
+            found.append(keyword)
+    return ", ".join(found)
+
+    
+today = datetime.now().strftime("%Y%m%d")
+
+db = SessionLocal()
+
+with sync_playwright() as p :
+    browser = p.chromium.launch(headless=False)
+    page = browser.new_page()
+
+    with open("data/jobs_웹개발_20260608.csv", "r", encoding="utf-8-sig") as f:
+        reader = csv.DictReader(f)
+        rows = list(reader)
+    print(f"총 {len(rows)}개 공고 크롤링 시작\n")
+
+    for i, row in enumerate(rows[:20]):
+        print(f"[{i+1}/{len(rows)}] {row['company']} - {row['title']}")
+
+        try :
+            detail_url = f"{BASE_URL}{row['href']}"
+            page.goto(detail_url)
+
+            page.wait_for_selector(".iframe_content", timeout=1000)
+
+            iframe_tag = page.query_selector(".iframe_content")
+            iframe_src = iframe_tag.get_attribute('src')
+            iframe_url = f"https://www.saramin.co.kr{iframe_src}"
+
+            page.goto(iframe_url)
+                
+            content = page.inner_text('body')
+
+            tech_stack = extract_keywords(content)
+
+            job = Job(
+                company=row["company"],
+                title=row['title'],
+                job_name="웹개발",#나중에 바꿔야함
+                content=content,
+                tech_stack=tech_stack,
+                href=row['href'],
+                date=today
+            )
+            db.add(job)
+            db.commit()
+            
+        except Exception as e:
+            print(f"-> 오류 :{e}")
+            db.rollback()
+            job = Job(
+                company=row["company"],
+                title=row['title'],
+                job_name="웹개발",#나중에 바꿔야함
+                content="",
+                tech_stack="",
+                href=row['href'],
+                date=today
+            )
+            db.add(job)
+            db.commit()
+    browser.close() 
+db.close()
